d83b98d69a2200a8316e117727f0e0814f20bd9c
horta88 <fernando43horta@gmail.com>
2026-02-10 20:38:23 -0300

    preparar  para deploy

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from database import criar_tabelas
 from controllers.rotas import biblioteca_bp
+import os
+
 
 app = Flask(__name__)
 
@@ -12,12 +14,9 @@ def home():
     return "OK"
 
 if __name__ == "__main__":
-    app.run(
-        host="127.0.0.1",
-        port=8081,
-        debug=False,
-        use_reloader=False
-    )
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
